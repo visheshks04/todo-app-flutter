5a3593a005af109e73a932535853ff05e4fa89d3
Vishesh Kumar Singh <visheshkrsinghofficial@gmail.com>
2023-04-05 10:27:31 +0530

    working fine

--- lib/main.dart
+++ lib/main.dart
@@ -3,20 +3,18 @@ import 'package:flutter/material.dart';
 import 'package:flutter_slidable/flutter_slidable.dart';
 import 'package:google_fonts/google_fonts.dart';
 import 'package:hive/hive.dart';
+import 'package:hive_flutter/adapters.dart';
 
-void main()  {
+Future<void> main()  async {
 
   // open a box
   
- hiveInit();
+ await hiveInit();
   runApp(const TodoApp());
 }
 Future<void> hiveInit() async{
- var path = Directory.current.path;
- Hive
- .init(path);
+ await Hive.initFlutter();
  var box= await Hive.openBox('mybox');
- 
 }
 class TodoApp extends StatelessWidget {
   const TodoApp({super.key});
@@ -65,16 +63,16 @@ class _HomeState extends State<Home> {
   
   List<String> tasks = [];
   List<bool> isChecked = [];
-  List<TextDecoration?> decoration = [];
-  TextEditingController addTaskController = new TextEditingController();
+  List<bool> linethrough = [];
+  TextEditingController addTaskController = TextEditingController();
   late final Box _listHiveBox;
   @override
-  void initState() {
+  void initState()  {
     hiveData();    
     super.initState();
   }
   Future<void> hiveData() async{
-   final _listHiveBox = await Hive.openBox('mybox');
+   _listHiveBox = await Hive.openBox('mybox');
    setState(() {
    if(!_listHiveBox.containsKey("TodoList")){
       createInitialData();
@@ -88,74 +86,70 @@ class _HomeState extends State<Home> {
   @override
   Widget build(BuildContext context) {
 
-   
-
     return Scaffold(
       body: Padding(
         padding: const EdgeInsets.all(20.0),
-        child: Container(
-          child: ListView.builder(
-            itemCount: tasks.length,
-            itemBuilder: (context, index) {
-              return Slidable(
-                child: ListTile(
-                  title: Text(
-                    tasks[index],
-                    style: TextStyle(
-                      decoration: decoration[index],
-                      color: Colors.white
-                    ),
-                  ),
-                  leading: Theme(
-                    child: Checkbox(
-                      value: isChecked[index],
-                      onChanged: (value) async {
-                        setState(() {
-                          isChecked[index] = value!;
-                          if(isChecked[index])
-                            decoration[index] = TextDecoration.lineThrough;
-                          else
-                            decoration[index] = null;
-                  
-                          updateData();
-                        });
-                      },
-                    ),
-                    data: ThemeData(
-                      unselectedWidgetColor: Colors.white
-                      
-                    ),
+        child: ListView.builder(
+          itemCount: tasks.length,
+          itemBuilder: (context, index) {
+            return Slidable(
+              startActionPane: ActionPane(
+                motion: const ScrollMotion(),
+                children: [
+                  SlidableAction(
+                    onPressed: (context) async {
+                      setState(() {
+                        tasks.removeAt(index);
+                        isChecked.removeAt(index);
+                        linethrough.removeAt(index);
+                        updateData();
+                      });
+                    },
+                    foregroundColor: Colors.black,
+                    backgroundColor: Colors.white,
+                    icon: Icons.delete,
+                  )
+                ],
+              ),
+              child: ListTile(
+                title: Text(
+                  tasks[index],
+                  style: TextStyle(
+                    decoration: linethrough[index]? TextDecoration.lineThrough:null,
+                    color: Colors.white
                   ),
                 ),
-                startActionPane: ActionPane(
-                  motion: const ScrollMotion(),
-                  children: [
-                    SlidableAction(
-                      onPressed: (context) async {
-                        setState(() {
-                          tasks.removeAt(index);
-                          isChecked.removeAt(index);
-                          decoration.removeAt(index);
-                          updateData();
-                        });
-                      },
-                      foregroundColor: Colors.black,
-                      backgroundColor: Colors.white,
-                      icon: Icons.delete,
-                    )
-                  ],
+                leading: Theme(
+                  data: ThemeData(
+                    unselectedWidgetColor: Colors.white
+                    
+                  ),
+                  child: Checkbox(
+                    value: isChecked[index],
+                    onChanged: (value) async {
+                      setState(() {
+                        isChecked[index] = value!;
+                        if(isChecked[index]) {
+                          linethrough[index] = true;
+                        } else{
+                          linethrough[index] = false;
+                        }
+                        updateData();
+                      });
+                    },
+                  ),
                 ),
-              );
-            },
-          ),
+              ),
+            );
+          },
         ),
       ),
       floatingActionButton: FloatingActionButton.extended(
         onPressed: (){
           showDialog(context: context, builder: (context){
             return AlertDialog(
-              icon: Icon(Icons.add),
-              title: Text("New Task"),
+              icon: const Icon(Icons.add),
+              title: const Text("New Task"),
               content: TextField(
                 controller: addTaskController,
               ),
@@ -164,42 +158,41 @@ class _HomeState extends State<Home> {
                   setState(() {
                     tasks.add(addTaskController.text);
                     isChecked.add(false);
-                    decoration.add(null);
+                    linethrough.add(false);
                     Navigator.of(context, rootNavigator: true).pop('dialog');
                     addTaskController.clear();
                     updateData();
                   });
-                }, child: Text("Add"),),
+                }, child: const Text("Add"),),
               ],
             );
           });
         },
-        label: Text("Add"),
-        icon: Icon(Icons.add),
+        label: const Text("Add"),
+        icon: const Icon(Icons.add),
         backgroundColor: Colors.white,
         // shape: BeveledRectangleBorder(borderRadius: BorderRadius.zero),
         
       ),
       backgroundColor: Colors.black,
-      floatingActionButtonLocation: FloatingActionButtonLocation.centerFloat,
     );
   }
   
   void createInitialData() {
     tasks = ["Practice Guitar","Water Plants", "Take the dog for a walk"];
     isChecked = [false, false, false];
-    decoration = [null, null, null];
+    linethrough = [false, false, false];
   }
   
-  void loadData() {
-    tasks = _listHiveBox.get("TodoList");
-    isChecked = _listHiveBox.get("isChecked");
-    decoration = _listHiveBox.get("decoration");
+  Future<void> loadData() async {
+    tasks = await _listHiveBox.get("TodoList");
+    isChecked = await _listHiveBox.get("isChecked");
+    linethrough = await  _listHiveBox.get("linethrough");
   }
 
   void updateData() async {
     await _listHiveBox.put("TodoList", tasks);
     await _listHiveBox.put("isChecked", isChecked);
-    await _listHiveBox.put("decoration", decoration);
+    await _listHiveBox.put("linethrough", linethrough);
   }
 }
